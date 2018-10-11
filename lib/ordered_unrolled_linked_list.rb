41027c0ede2c6c933d3ac6e3d4c9b0bb652e8a8c
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-11 10:50:51 -0400

    First commit

---
+++ lib/ordered_unrolled_linked_list.rb
@@ -0,0 +1,5 @@
+require "ordered_unrolled_linked_list/version"
+
+module OrderedUnrolledLinkedList
+  # Your code goes here...
+end
---
+++ lib/ordered_unrolled_linked_list/version.rb
@@ -0,0 +1,3 @@
+module OrderedUnrolledLinkedList
+  VERSION = "0.1.0"
+end
