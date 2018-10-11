d254ff41ed0cd15c3eff08c89ad8eb3376a775fd
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-11 12:39:16 -0400

    Wip

--- lib/ordered_unrolled_linked_list/list.rb
+++ lib/ordered_unrolled_linked_list/list.rb
@@ -1,11 +1,23 @@
 module OrderedUnrolledLinkedList
   class List
-    def initialize(data = [])
-      @list = initialize_new_list(data)
+    DEFAULT_MAX_NODE_LENGTH = 5
+
+    attr_reader :max_node_length, :list, :head
+
+    def initialize(attributes = {})
+      attributes[:data]  ||= []
+      @max_node_length = attributes[:max_node_length] || DEFAULT_MAX_NODE_LENGTH
+      @head = Node.new
+      @list = initialize_new_list(attributes[:data])
     end
 
     def initialize_new_list(data)
       data.each { |e| add(e) }
+    end
+
+    def add(data)
+      cur_node = @head
+      # find the array that the item needs to be in
     end
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -3,7 +3,16 @@ module OrderedUnrolledLinkedList
   RSpec.describe List do
     describe 'new' do
       it 'creates a new list' do
-      list = described_class.new([1,2,3])
+        list = described_class.new
+        expect(list.max_node_length).to eq(5)
+      # list = described_class.new({data: [1,2,3]})
+      end
+
+      describe 'with data' do
+        it 'creates a head node with the data' do
+          list = described_class.new({ data: [1,2,3] })
+          expect(list.head.data).to eq([1,2,3])
+        end
       end
     end
   end
