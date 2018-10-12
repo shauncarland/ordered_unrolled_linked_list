0a45b9f710ce54ff2aa6320b140200ffb84916eb
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-12 15:41:53 -0400

    Get insert_new_node method working

--- lib/ordered_unrolled_linked_list/list.rb
+++ lib/ordered_unrolled_linked_list/list.rb
@@ -34,6 +34,7 @@ module OrderedUnrolledLinkedList
       insert_in_node(e, cur_node)
     end
 
+    private
     def insert_in_node(e, node)
       node.data << e
       node.data.sort!
@@ -42,12 +43,27 @@ module OrderedUnrolledLinkedList
         next_node = node.next
 
         new_node = Node.new({ data: [node.data.pop] })
+        insert_new_node(node, new_node)
 
-        node.next = new_node
-        new_node.prev = node
-        new_node.next = next_node
-        next_node.prev = new_node unless next_node.nil?
+        # node.next = new_node
+        # new_node.prev = node
+        # new_node.next = next_node
+        # next_node.prev = new_node unless next_node.nil?
       end
+    end
+
+    def insert_new_node(node, new_node)
+      next_node = node.next
+
+      node.next = new_node
+      new_node.prev = node
+      new_node.next = next_node
+      next_node.prev = new_node unless next_node.nil?
+
+      # prev_node_next = prev_node.next
+      # prev_node.next = new_node
+      # new_node.prev = prev_node
+      # new_node.next = prev_node_next unless prev_node_next.nil?
     end
   end
 end
