cd01ed6e4a352fe18e3397d9c30b99e4dc2a2797
Shaun Carland <thestarsareprojectors824@gmail.com>
2018-10-12 14:02:03 -0400

    insert_in_node logic

--- lib/ordered_unrolled_linked_list/list.rb
+++ lib/ordered_unrolled_linked_list/list.rb
@@ -15,9 +15,40 @@ module OrderedUnrolledLinkedList
       data.each { |e| add(e) }
     end
 
-    def add(data)
+    def add(e)
+      if @head.data.empty?
+        @head.data << e
+        return
+      end
+
       cur_node = @head
-      # find the array that the item needs to be in
+
+      while !cur_node.nil? do
+        if cur_node.data.min >= e && cur_node.data.max <= e
+          insert_in_node(e, cur_node)
+          return
+        else
+          cur_node = cur_node.next
+        end
+      end
+    end
+
+    def insert_in_node(e, node)
+      node.data << e
+      node.data.sort!
+
+      if node.data.length > @max_node_length
+        next_node = node.next
+
+        # node -> next_node
+        # node -> new_node -> next_node
+        new_node = Node.new({ data: [node.data.pop] })
+
+        node.next = new_node
+        new_node.prev = node
+        new_node.next = next_node
+        next_node.prev = new_node unless next_node.nil?
+      end
     end
   end
 end
--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -1,19 +1,43 @@
 require 'ordered_unrolled_linked_list'
 module OrderedUnrolledLinkedList
   RSpec.describe List do
-    describe 'new' do
+    describe '#new' do
       it 'creates a new list' do
         list = described_class.new
         expect(list.max_node_length).to eq(5)
-      # list = described_class.new({data: [1,2,3]})
       end
 
-      describe 'with data' do
-        it 'creates a head node with the data' do
-          list = described_class.new({ data: [1,2,3] })
-          expect(list.head.data).to eq([1,2,3])
+      # describe 'with data' do
+      #   it 'creates a head node with the data' do
+      #     list = described_class.new({ data: [1,2,3] })
+      #     expect(list.head.data).to eq([1,2,3])
+      #   end
+      # end
+    end
+
+    describe '#add' do
+      let(:list) { described_class.new }
+      describe 'adding to an empty list' do
+        it 'adds the item to the head node' do
+          list.add(1)
+          expect(list.head.data).to eq([1])
+        end
+
+        describe 'adding beyond the head nodes capacity' do
+          it 'creates a new node' do
+            6.times { list.add(1) }
+            expect(list.head.next.data).to eq([1])
+          end
         end
       end
+      # describe 'when adding to an unfilled head node' do
+      #   let(:list) { described.class.new([1,2,4]) }
+      #
+      #   it 'adds an item to the head node in the correct order' do
+      #     list.add(3)
+      #     expect(list.head.data).to eq([1,2,3,4])
+      #   end
+      # end
     end
   end
 end
